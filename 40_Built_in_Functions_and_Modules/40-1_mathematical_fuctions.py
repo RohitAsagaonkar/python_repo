5e76cea3804b26844d2e2683e1e23182a7be421a
RohitAsagaonkar <rohitasagaonkar@gmail.com>
2026-01-05 14:39:08 +0530

    add third argument

--- 40_Built_in_Functions_and_Modules/40-1_mathematical_fuctions.py
+++ 40_Built_in_Functions_and_Modules/40-1_mathematical_fuctions.py
@@ -2,3 +2,5 @@
 print(abs(-70)) #this function gives the posotive value of the given number 
 
 print(pow(2,3)) #it gives the power of number 
+
+print(pow(10,2,3)) #this will give mod also 100 moduls 3 =           1
