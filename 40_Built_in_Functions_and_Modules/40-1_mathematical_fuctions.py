8f4512034dcc32a59e65f3ba89fcfea2a02515ae
RohitAsagaonkar <rohitasagaonkar@gmail.com>
2026-01-10 13:24:37 +0530

    peace of code fuction

--- 40_Built_in_Functions_and_Modules/40-1_mathematical_fuctions.py
+++ 40_Built_in_Functions_and_Modules/40-1_mathematical_fuctions.py
@@ -1,6 +1,27 @@
 # Abolute function
-print(abs(-70)) #this function gives the posotive value of the given number 
+print("this is absolute function" , abs( -70)) #this function gives the posotive value of the given number 
+# power function 
+print("this is power  ",pow(2,3)) #it gives the power of number 
 
-print(pow(2,3)) #it gives the power of number 
+print("this is power used for mod",pow(10,2,3)) #this will give mod also 100 moduls 3 =           1
 
-print(pow(10,2,3)) #this will give mod also 100 moduls 3 =           1
+#rounding
+print("this is rounding result"  ,round(5.5)) #its is called as bakers rounding
+print("this is rounding result"  ,round(4.5)) #its is called as bakers rounding
+print("this is rounding result"  ,round(3.564465656)) #no round condition its is called as bakers rounding
+print("this is rounding result"  ,round(3.564465656,2)) # it is because of two rond ahter dotits is called as bakers rounding 
+
+#divmod
+print("this is divemode quotient and remender", divmod(10,3))
+
+#minimum finding (min)
+a=min(12,34,56,34,5,6,75,345,34555,5,6,.6,45,-19)
+print("this is minimum answer",a)
+#max
+print("it finds maximum number",max(12,34,5465,567857,56465654,67765,653,657,5767,76,54,76548787865,6576,57,65,67,6,5765,65,765,56,78,77,87,7,77,7,7,777777,7,7,77))
+
+#sum
+print('''it add the all elements''',([1,2,5,4,3,76,54,78,9,7834]))
+
+#eval
+print("this solves the expression",eval("12+32+43*43-6565/32"))
