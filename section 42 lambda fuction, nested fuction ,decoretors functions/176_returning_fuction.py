22794790446eb096934a03730a2243bd45c0b6db
RohitAsagaonkar <rohitasagaonkar@gmail.com>
2026-01-15 10:57:10 +0530

    returning fuction

---
+++ section 42 lambda fuction, nested fuction ,decoretors functions/176_returning_fuction.py
@@ -0,0 +1,11 @@
+"""outer fuction runs only one time dose not run two times hense i called f() three times"""
+def outer ():
+    print("car")
+    def inner():
+        print("welcome")
+        
+    return inner # here inner fuction object (all code of it) is returned not inner fuction run and return
+f=outer()  #here f is storing exicuted outer and outer was returning inner and now f has stored inner fuction object (code)
+f() # f now becamed the inner because it has inner code then we can call directly inner like this means we are calomh the fuction
+f()
+f()
